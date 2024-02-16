8db410ff02d132793ced6a607ac66ca9e02b6db6
Jwilv <juan.ignacio.wilvers@gmail.com>
2024-02-16 13:10:36 -0300

    create usePagination

--- app/index.tsx
+++ app/index.tsx
@@ -1,23 +1,54 @@
 import { Stack, Link } from 'expo-router';
-import { YStack } from 'tamagui';
+import { XStack, YStack, View } from 'tamagui';
 
 import { Container, Main, Subtitle, Button, ButtonText } from '../tamagui.config';
 import { Text } from 'react-native';
+import { useEffect, useState } from 'react';
+import { Breweries } from '~/types';
+import { usePagination } from '~/hooks/usePagination';
 
 export default function Page() {
+
+  const { breweriesPage, pages, currentPage, changePage } = usePagination();
+
   return (
     <Container>
+      <Text>{currentPage}</Text>
       <Main>
-        <Stack.Screen  />
-        <YStack>
-          <Text>Hello World</Text>
-          <Subtitle>This is the first page of your app.</Subtitle>
-        </YStack>
-        <Link href={{ pathname: '/brewery/1' }} asChild>
-          <Button>
-            <ButtonText>Show Details</ButtonText>
+        {
+          breweriesPage.map((brewery: any) => {
+            return (
+              <YStack key={brewery.id}>
+                <Text>{brewery.name}</Text>
+              </YStack>
+            )
+          })
+        }
+        <View display='flex' flexDirection='row'>
+          <Button
+            padding={0}
+            onTouchEnd={() => changePage(-1)}
+          >
+            <ButtonText>Previous</ButtonText>
           </Button>
-        </Link>
+          {
+            pages.map((page) => (
+              <XStack key={page} margin={5}>
+                {
+                    <Button>
+                      <ButtonText>{page}</ButtonText>
+                    </Button>
+                }
+              </XStack>
+            ))
+          }
+          <Button
+            padding={0}
+            onTouchEnd={() => changePage(1)}
+          >
+            <ButtonText>Next</ButtonText>
+          </Button>
+        </View>
       </Main>
     </Container>
   );
---
+++ hooks/usePagination.ts
@@ -0,0 +1,61 @@
+import { useEffect, useState } from "react"
+import { Breweries } from "~/types"
+
+
+export const usePagination = () => {
+    const [breweries, setBreweries] = useState<Breweries>([]);
+
+    const [pages, setPages] = useState([1, 2, 3, 4, 5]);
+
+    const [currentPage, setCurrentPage] = useState(1);
+
+    const [pageUpdate, setPageUpdate] = useState(2);
+
+    const [breweriesPage, setBreweriesPage] = useState<Breweries>([]);
+
+    const changePage = (addAndSubstract: number) => {
+        if ((currentPage + addAndSubstract) >= 1) {
+            setCurrentPage(currentPage + addAndSubstract);
+        }
+        if (pages[0] + addAndSubstract >= 1) {
+            setPages(pages.map((page) => page + addAndSubstract));
+        }
+    }
+
+    useEffect(() => {
+        const fechtUpdate = async () => {
+            if (currentPage % 5 === 0) {
+                const response = await fetch(`https://api.openbrewerydb.org/v1/breweries?page=${pageUpdate}&per_page=88`);
+                const data: Breweries = await response.json();
+                setBreweries([...breweries, ...data]);
+                setPageUpdate(pageUpdate + 1);
+            }
+        }
+
+        fechtUpdate();
+
+    }, [currentPage])
+
+    //update data page
+    useEffect(() => {
+        setBreweriesPage(breweries.slice(currentPage * 8, (currentPage + 1) * 8));
+    }, [currentPage, breweries])
+
+    //initial load data from api
+    useEffect(() => {
+        const fecthData = async () => {
+            const response = await fetch('https://api.openbrewerydb.org/v1/breweries?per_page=88');
+            const data: Breweries = await response.json();
+            setBreweries(data)
+        }
+
+        fecthData()
+    }, [])
+
+    return {
+        changePage,
+        currentPage,
+        breweriesPage,
+        pages,
+    }
+}
