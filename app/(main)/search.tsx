8d7ad07228b1ffcf8d798190f5f85b2a6f2642d6
Jwilv <juan.ignacio.wilvers@gmail.com>
2024-02-19 13:11:56 -0300

    refactor in SearchPage and SearchBrewery

--- app/(main)/search.tsx
+++ app/(main)/search.tsx
@@ -1,12 +1,45 @@
+import { useEffect, useState } from "react";
 import { SearchBrewery } from "~/components/breweries/SearchBrewery"
+import { useDebonce } from "~/hooks";
+import breweriesApi from "~/services/breweries";
 import { Container, Main } from "~/tamagui.config"
+import { Brewery } from "~/types";
 
-
+/**
+ * This function represents the SearchPage component, which handles the search functionality for breweries.
+ *
+ * @return {JSX.Element} The JSX element representing the SearchPage component.
+ */
 const SearchPage = () => {
+
+    const [inputValue, setinputValue] = useState('');
+    const debouncedValue = useDebonce(inputValue, 500);
+    const [breweriesData, setBreweriesData] = useState<Brewery[]>([]);
+
+    useEffect(() => {
+
+        const getBereweriesByName = async () => {
+            try {
+                const { data } = await breweriesApi.get(`?by_name=${debouncedValue}&per_page=8`);
+                setBreweriesData(data)
+            } catch (error) {
+                console.log(error)
+            }
+        }
+
+        inputValue ? getBereweriesByName() : setBreweriesData([])
+
+    }, [debouncedValue])
+
     return (
         <Container>
             <Main alignItems='center'>
-                <SearchBrewery />
+                <SearchBrewery
+                    inputValue={inputValue}
+                    handleChangeValue={setinputValue}
+                    breweries={breweriesData}
+                    debouncedValue={debouncedValue}
+                />
             </Main>
         </Container>
     )
