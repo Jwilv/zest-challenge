33c2b6bc66c91c14d534ff7c0445bf2f3fc6a257
Jwilv <juan.ignacio.wilvers@gmail.com>
2024-02-18 21:25:31 -0300

    fix : fix bug active false

--- components/navigation/components/TabItem.tsx
+++ components/navigation/components/TabItem.tsx
@@ -1,25 +1,26 @@
-import { Href, Link, LinkProps } from 'expo-router';
+import { Href, Link, usePathname } from 'expo-router';
 import { LucideIcon } from 'lucide-react-native';
 import React from 'react'
 import { TouchableOpacity } from 'react-native'
 import { BarItem } from '~/tamagui.config'
 
 interface Props {
-    isActive: boolean;
     href: Href<string>;
     icon: LucideIcon;
 }
 
 export const TabItem = ({
-    isActive,
     href,
     icon: Icon,
 }: Props) => {
+
+    const pathname = usePathname();
+
     return (
         <Link href={{ pathname: href }} asChild>
             <TouchableOpacity>
-                <BarItem backgroundColor={isActive ? '#007AFF' : '$colorTransparent'}>
-                    <Icon size={27} color={isActive ? '#fff' : '#007AFF'} />
+                <BarItem backgroundColor={pathname === href ? '#007AFF' : '$colorTransparent'}>
+                    <Icon size={27} color={pathname === href ? '#fff' : '#007AFF'} />
                 </BarItem>
             </TouchableOpacity>
         </Link>
